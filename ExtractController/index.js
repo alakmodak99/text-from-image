b0c1f579324fcc97748522336bd91746a94eb69c
alakmodak99 <alakmodak99@gmail.com>
2023-05-23 19:33:47 +0530

    minor fixes

--- ExtractController/index.js
+++ ExtractController/index.js
@@ -3,6 +3,12 @@ const router = express.Router();
 var Tesseract = require("tesseract.js");
 router.post("/", async (req, res) => {
   try {
+    if (!req?.body?.imageUrl) {
+      res.status(400).send({
+        message: "Image url is required",
+      });
+      return;
+    }
     let data = await Tesseract?.recognize(req?.body?.imageUrl, "eng");
     res?.status(201).send({
       message: "success",
